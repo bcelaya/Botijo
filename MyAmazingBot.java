bc29de47b0107d5c7c0024307e060c41a01dc5a3
Bruno Celaya Montero <brunozelaia@gmail.com>
2018-06-21 16:58:22 +0200

    Ficheros añadidos mediante el gestor de archivos
    Experimentando para que envie las fotos que yo quiero, próximo a hacer, incluir métodos para los valores de BitCoin , etc y también que haga de asistente para el juego Criptominati (te cree él el usuario)

---
+++ MyAmazingBot.java
@@ -0,0 +1,46 @@
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+public class MyAmazingBot extends TelegramLongPollingBot {
+   @Override
+   public void onUpdateReceived(Update update) {
+      
+       // We check if the update has a message and the message has text
+       if (update.hasMessage() && update.getMessage().hasText()) {
+           // Set variables
+           String message_text = update.getMessage().getText();
+           long chat_id = update.getMessage().getChatId();
+           if (message_text.equals("Dafne")) {
+        	   message_text = "Hola Dafnecita rebonita preciosita! :-****";
+           }
+           else
+        	   if (message_text.equals("criptominati")) {
+        		   message_text = "Bienvenido al menu de juego de Criptominati";
+        	   }
+           
+           SendMessage message = new SendMessage() // Create a message object object
+               .setChatId(chat_id)
+               .setText(message_text);
+           try {
+               execute(message); // Sending our message object to user
+           } catch (TelegramApiException e) {
+               e.printStackTrace();
+           }
+       }
+   }
+
+   @Override
+   public String getBotUsername() {
+       // Return bot username
+       // If bot username is @MyAmazingBot, it must return 'MyAmazingBot'
+       return "brunozelaiabot";
+   }
+
+   @Override
+   public String getBotToken() {
+       // Return bot token from BotFather
+       return "617944826:AAESAQU2OG9nZYy1ZI4QfoTQRNT9_-jpvD0";
+   }
+}
